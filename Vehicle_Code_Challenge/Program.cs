5dca1ce7b24082ded6b3b5070abce1b6b8d36c26
J <J@JPC>
2021-12-03 05:19:58 -0700

    Add project files.

---
+++ Vehicle_Code_Challenge/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public interface IVehicle
+{
+    string Color { get; set; }
+    string Manufacturer { get; set; }
+    string Model { get; set; }
+    int YearBuilt { get; set; }
+}
